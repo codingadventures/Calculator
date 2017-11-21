da00bcd8ed6662adfb5bb28d3126927e95101333
SuperG <nbasakuragi@gmail.com>
2017-11-21 21:37:31 +0000

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Calculator!");
+            Console.WriteLine("Please Insert your operation and press enter");
+
+            string operations = Console.ReadLine();
+
+            var numbers = operations.Split('+').ToList();
+
+
+            Console.WriteLine($"The sum is: {numbers.Select(int.Parse).Sum()}2+ ");
+            Console.ReadLine();
+        }
+    }
+}
