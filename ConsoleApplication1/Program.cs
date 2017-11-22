c7e0aecab0cc64979f1cdf12f053e52b5f4b50a7
SuperG <nbasakuragi@gmail.com>
2017-11-22 21:38:50 +0000

    AC-9 AC-10 AC-11
    fixes the bugs as per jira tickets

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,15 +9,35 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Calculator!");
+		REDO:
             Console.WriteLine("Please Insert your operation and press enter");
 
             string operations = Console.ReadLine();
 
-            var numbers = operations.Split('+').ToList();
+			var numbers = operations.Split('+').ToList();
 
+			if (!numbers.All (p => int.TryParse (p, out int i)))
+			{
+				Console.WriteLine ("Letters are not allowed");
+				Console.Clear ();
+				goto REDO;
+			}
 
-            Console.WriteLine($"The sum is: {numbers.Select(int.Parse).Sum()}2+ ");
-            Console.ReadLine();
+			if (numbers.Count > 2) 
+			{
+				int buffer = 0;
+				for (int i = 0; i < numbers.Count ; i++) 
+				{
+					Console.Write ("Sum of " + buffer + " and " + numbers [i] + "is: ");
+					 
+					buffer += int.Parse (numbers [i]) - 1;
+
+					Console.WriteLine (buffer);
+				}
+			}else
+            	Console.WriteLine($"The sum is: {numbers.Select(int.Parse).Sum()}");
+            
+			Console.ReadLine();
         }
     }
 }
