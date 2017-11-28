daedb52b822d2254b56a50730a6abbc7b4a2d5a2
SuperG <nbasakuragi@gmail.com>
2017-11-28 22:09:15 +0000

    implements add and sub

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,88 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 
 namespace ConsoleApplication1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Calculator!");
-		 
-			Console.WriteLine("Please Insert your operation and press enter");
+	class Program
+	{
+		enum Operation
+		{
+			None,
+			Add,
+			Sub,
+			Mul,
+			Div,
+			Perc
+		}
+		static void Main (string [] args)
+		{
+			Console.WriteLine ("Welcome to Calculator!");
 
-			List<string> numbers = new List<string>();
-			List<byte> acc = new List<byte> ();
+			Console.WriteLine ("Please Insert your operation and press enter");
+
+			var operations = new Queue<string> ();
+
+
+			//List<string> numbers = new List<string>();
+			List<byte> numberAccumulator = new List<byte> ();
 			//Stack<byte> accumulator = new Stack<byte>;
 
-			float sum = 0;
-					
-			while(true)
-			{
+			float intermediateResult = 0;
+
+			while (true) {
 				int console = Console.Read ();
 				if (console <= 57 && console >= 48 || console == 46) {
 					//number 0..9 and .
-					acc.Add ((byte)console);
+					numberAccumulator.Add ((byte)console);
 					continue;
 				}
 
 				if (console == 43) // Add
 				{
 					Console.WriteLine ("\b"); //cancel + and create a new line
-					var res = System.Text.ASCIIEncoding.ASCII.GetString (acc.ToArray());
-					acc.Clear ();
-					numbers.Add (res);
-					float value;
-					float.TryParse (res, out value);
-					sum += value;
+					var number = System.Text.ASCIIEncoding.ASCII.GetString (numberAccumulator.ToArray ());
+					numberAccumulator.Clear ();
+					operations.Enqueue (number);
 
-					if(numbers.Count > 1)
-					Console.WriteLine (sum);
+					if (operations.Count == 3) // means it needs an intermidiate result
+					{
+						intermediateResult = Parse (operations);
+						Console.WriteLine (intermediateResult);
+						operations.Enqueue (intermediateResult.ToString ());
+					}
+					operations.Enqueue ("Add");
+
 					Console.WriteLine ('+');
-					
+
 					continue;
 				}
 
-				if (console == 10 || console == 61) { 
-					//enter or equal
-					var res = System.Text.ASCIIEncoding.ASCII.GetString (acc.ToArray ());
-					acc.Clear ();
-					numbers.Add (res);
-					foreach (var number in numbers) {
-						float value;
-						float.TryParse (number, out value);
-						sum += value;
-					}
-					numbers.Clear ();
-					Console.WriteLine (sum);
+				if (console == 10 || console == 61) {
+					var number = System.Text.ASCIIEncoding.ASCII.GetString (numberAccumulator.ToArray ());
+					numberAccumulator.Clear ();
+					operations.Enqueue (number);
+					Console.WriteLine (Parse(operations));
 					continue;
+				}
+
+				if (console == 45) //sub
+				{
+					Console.WriteLine ("\b"); //cancel - and create a new line
+					var number = System.Text.ASCIIEncoding.ASCII.GetString (numberAccumulator.ToArray ());
+					numberAccumulator.Clear ();
+					operations.Enqueue (number);
+
+					if (operations.Count == 3) // means it needs an intermidiate result
+					{
+						intermediateResult = Parse (operations);
+						Console.WriteLine (intermediateResult);
+						operations.Enqueue (intermediateResult.ToString());
+					}
+					operations.Enqueue ("Sub");
+					
+					Console.WriteLine ('-');
 				}
 				Console.Write ("\b");
 			}
+		}
 
-			//if (!numbers.All (p => int.TryParse (p, out int i) || float.TryParse(p, out float f)))
-			//{
-			//	Console.WriteLine ("Letters are not allowed");
-			//	Console.Clear ();
-			//	goto REDO;
-			//}
+		static float Parse (Queue<string> operations)
+		{
+			float [] result = new float [2];
+			int index = 0;
+			Operation type = Operation.None;
 
-			//if (numbers.Count > 2) 
-			//{
-			//	int buffer = 0;
-			//	for (int i = 0; i < numbers.Count ; i++) 
-			//	{
-			//		Console.Write ("Sum of " + buffer + " and " + numbers [i] + "is: ");
-					 
-			//		buffer += int.Parse (numbers [i]) - 1;
+			while (operations.Count >0) {
+				string operation = operations.Dequeue ();
+				float intermRes;
+				if (float.TryParse (operation, out intermRes)) {
+					result [index++] = intermRes;
+				} else {
+					//it's an operator
+					type = (Operation)Enum.Parse (typeof (Operation), operation);
+				}
+			}
 
-			//		Console.WriteLine (buffer);
-			//	}
-			//}else
-   //         	Console.WriteLine($"The sum is: {numbers.Select(int.Parse).Sum()}");
-            
-			//Console.ReadLine();
-        }
-    }
+			switch (type) {
+			case Operation.Add:
+				return result [0] + result [1];
+			case Operation.Sub:
+				return result [0] - result [1];
+			case Operation.Mul:
+				return result [0] * result [1];
+			case Operation.Div:
+				return result [0] / result [1];
+			case Operation.Perc:
+				break;
+			}
+
+			throw new Exception ("No operation available");
+		}
+	}
 }
