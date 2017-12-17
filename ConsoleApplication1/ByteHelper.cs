b7ec45fea2ad88f26974cc5c583fda86350fe008
SuperG <nbasakuragi@gmail.com>
2017-12-17 17:21:38 +0000

    adds git ignore

---
+++ ConsoleApplication1/ByteHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApplication1
+{
+    internal partial class Parser
+    {
+        private static class ByteHelper
+        {
+            public static bool IsNumber(this byte number)
+            {
+                return number <= 57 && number >= 48;
+            }
+        
+            public bool IsOperation(byte operation)
+            {
+                return _operatorsAsciiKeys.Contains(operation);
+            } 
+        }
+    }
+}
